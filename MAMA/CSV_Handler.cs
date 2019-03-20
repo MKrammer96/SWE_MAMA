ff61fbe831d728daef4dfdddf62ebe207e2863da
Manuel Krammer <Manuel.Krammer@students.fh-wels.at>
2019-03-20 12:06:15 +0100

    Tried to do something in CSV

--- MAMA/CSV_Handler.cs
+++ MAMA/CSV_Handler.cs
@@ -10,21 +10,33 @@ namespace MAMA
 {
     public class CSV_Handler
     {
-        private Excel.Workbook _MyBook = null;
-        private Excel.Application _MyApp = null;
-        private Excel.Worksheet _MySheet = null;
+        private Excel.Workbook _myBook = null;
+        private Excel.Application _myApp = null;
+        private Excel.Worksheet _mySheet = null;
 
         public CSV_Handler()
         {
-            Excel.Application xlApp = new Excel.Application();
-            Excel.Workbook xlWorkbook = xlApp.Workbooks.Open(@"MAMA.xlsx");
-            Excel.Worksheet xlWorksheet = xlWorkbook.Sheets[1];
-            Excel.Range xlRange = xlWorksheet.UsedRange;
+            
         }
 
         public void exportToExcel(DataGridView data)
         {
-            
+            _myApp = new Excel.Application();
+            _myApp.Visible = true;
+
+            _myBook = _myApp.Workbooks.Add();
+
+            _mySheet = _myBook.Worksheets[1];
+
+            _mySheet.Cells[1, 1] = "ID";
+            _mySheet.Cells[2, 1] = "Last Name";
+            _mySheet.Cells[3, 1] = "First Name";
+            _mySheet.Cells[4, 1] = "Date of Change";
+            _mySheet.Cells[5, 1] = "Current Balance";
+            _mySheet.Cells[6, 1] = "Password";
+
+            string filename = "C:/Users/Mani/Desktop";
+            _myBook.SaveAs(filename);
         }
 
     }
