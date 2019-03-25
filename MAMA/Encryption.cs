06fff701c707699c54938bf91193ff0ad68bb353
Manuel Krammer <Manuel.Krammer@students.fh-wels.at>
2019-03-25 16:55:48 +0100

    Password, Encrypt hinzugefügt

--- MAMA/Encryption.cs
+++ MAMA/Encryption.cs
@@ -8,86 +8,52 @@ namespace MAMA
 {
     class Encryption
     {
-        //static void Main(string[] args)
-        //{
-        //    String cPassPhrase;
-        //    String cTextIn;
+        private string _encryptedText;
+        AesCryptoServiceProvider aesCSP;
 
-        //    // get input 
-        //    Console.WriteLine("Input text to encrypt");
-        //    cTextIn = Console.ReadLine();
-        //    do
-        //    {
-        //        Console.WriteLine("Input a pass phrase (like a key) - with 8 characters");
-        //        cPassPhrase = Console.ReadLine();
-        //    } while (cPassPhrase.Length != 8); // based on DES block size
-
-        //    // do it
-        //    encrypt(cTextIn, cPassPhrase);
-
-        //    // check it
-        //    String cTest = decrypt(cPassPhrase);
-        //    if (cTest == cTextIn)
-        //    {
-        //        Console.WriteLine("Perfectly reconstructed");
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("There seems to be a problem...");
-        //    }
-        //    Console.WriteLine("Test: {0}", cTest);
-        //}
-
-        public static void encrypt(String cTextIn, String cPassPhrase)
+        /// <summary>
+        /// Creates a new Crypto Provider
+        /// </summary>
+        /// <param name="userinput">Select your Password</param>
+        public Encryption(string userinput)
         {
-            // write output to a file
-            byte[] buffer = new byte[100];
-            FileStream fencrypted = new FileStream(@"c:\temp\encryp.txt", FileMode.OpenOrCreate, FileAccess.Write);
-            fencrypted.SetLength(0); // empty any old contents
+            aesCSP = new AesCryptoServiceProvider();
 
-            DES des = new DESCryptoServiceProvider();
-            des.Key = Encoding.ASCII.GetBytes(cPassPhrase);
-            des.IV = Encoding.ASCII.GetBytes(cPassPhrase); // use same
-            CryptoStream encStream = new CryptoStream(fencrypted, des.CreateEncryptor(), CryptoStreamMode.Write);
+            aesCSP.GenerateKey();
+            aesCSP.GenerateIV();
 
-            // encrypt text from parameter and write to the file
-            // - CryptoStream linked to File, so write to CryptoStream
-            // - CryptoStream basically bytes (remember so are chars)
-            Console.WriteLine("Encrypting...");
-            buffer = Encoding.ASCII.GetBytes(cTextIn);
-            encStream.Write(buffer, 0, cTextIn.Length);
-
-            encStream.Close();
-            fencrypted.Close();
+            _encryptedText = encrpyt(userinput);
         }
 
-        public static String decrypt(String cPassPhrase)
+
+        private byte[] EncryptString(SymmetricAlgorithm symAlg, string inString)
         {
-            String cTextOut;
-            const int bufsize = 100;
-            byte[] buffer = new byte[bufsize];
+            byte[] inBlock = UnicodeEncoding.Unicode.GetBytes(inString);
+            ICryptoTransform xfrm = symAlg.CreateEncryptor();
+            byte[] outBlock = xfrm.TransformFinalBlock(inBlock, 0, inBlock.Length);
 
-            // text to decrypt
-            FileStream fencrypted = new FileStream(@"c:\temp\encryp.txt", FileMode.Open, FileAccess.Read);
+            return outBlock;
+        }
 
-            DES des = new DESCryptoServiceProvider();
-            des.Key = Encoding.ASCII.GetBytes(cPassPhrase);
-            des.IV = Encoding.ASCII.GetBytes(cPassPhrase); // use same
-            CryptoStream encStream = new CryptoStream(fencrypted, des.CreateDecryptor(), CryptoStreamMode.Read);
+        /// <summary>
+        /// Returns the encrypted Password
+        /// </summary>
+        /// <returns></returns>
+        public string getEncryptedText()
+        {
+            return _encryptedText;
+        }
 
-            //decrypt and store in string
-            Console.WriteLine("Decrypting...");
-            encStream.Read(buffer, 0, bufsize);
-            cTextOut = Encoding.ASCII.GetString(buffer, 0, (int)fencrypted.Length);
+        /// <summary>
+        /// Encrypts your Input
+        /// </summary>
+        /// <param name="userinput">Userinput</param>
+        /// <returns></returns>
+        public string encrpyt(string userinput)
+        {
+            byte[] encQuote = EncryptString(aesCSP, userinput);
 
-            // since always encodes in blocks, may have extra chars at the end
-            int end = cTextOut.IndexOf('\0');
-            cTextOut = cTextOut.Substring(0, end);
-
-            encStream.Close();
-            fencrypted.Close();
-
-            return cTextOut;
+            return Convert.ToBase64String(encQuote);
         }
     }
 
